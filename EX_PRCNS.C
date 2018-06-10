2f43bc1ed94e9b2976d44decd9acb5ad9c032c18
Vicente Lobo <vicenteclobo+git@gmail.com>
2018-06-10 15:08:17 -0300

    Adição de comentários e teste de generalização do produtor/consumidor.

--- EX_PRCNS.C
+++ EX_PRCNS.C
@@ -2,43 +2,93 @@
 #include <prodcons.h>
 
 FILE *input, *output, *log;
+/* Long unsigned porque tamanho do arquivo pode ser muito grande, aí não caberia no int. */
 long unsigned int size, max_buffer, pos_atual = 0, it_serv = 0;
+/* Unsigned char para não perder dados do bit mais significativo, 
+   pois quando a variável é signed, o último bit é usado pra isso (o sinal). */
 unsigned char *download_servidor;
 
+/* Função que será chamada pelo produtor (produz) */
 unsigned char far servidor() {
 	unsigned char novo_byte;
+	/* 	Salva o próximo byte lido de input em novo_byte.
+		aqui, na verdade, poderia substituir o sizeof(unsigned char) por 1, já que
+		um unsigned char tem exatamente um byte de tamanho, mas não coloquei porque
+		como estava dando erro, talvez pudesse ser disso. */
 	fread(&novo_byte, sizeof(unsigned char), 1, input);
+	/* Salvar no log qual o byte escrito no buffer. */
 	fprintf(log, "Servidor %d: %02x\n", it_serv++, novo_byte);
 	return novo_byte;
 }
 
+/* Função que será chamada pelo consumidor (consome) */
 void far cliente(unsigned char novo_byte) {
+	/* Salvar no log qual o byte lido. */
 	fprintf(log, "Cliente %d: %02x\n", pos_atual, novo_byte);
+	/* Salvar no arquivo de saída o dado lido e avançar o iterador para próxima posição*/
 	download_servidor[pos_atual++] = novo_byte;
 }
 
+/*  Definir tamanho do arquivo, do buffer usado pelo prod/cons 
+	e alocação do buffer que salvará as informações recebidas pelo cliente. */
 void far configurar() {
+	/* Posiciona ponteiro de leitura no final do arquivo */
 	fseek(input, 0, SEEK_END);
+	/* Vê em qual byte o ponteiro está. Isso é usado para saber o tamanho do arquivo. */
 	size = ftell(input);
+	/* Volta ponteiro de leitura ao início do arquivo para poder começar a leitura. */
 	fseek(input, 0, SEEK_SET);
 	max_buffer = size / 100;
 	printf("%lu\n", size);
 	download_servidor = (unsigned char *) calloc (size, sizeof(unsigned char));
 }
 
+/*  Chamada pelo consumidor após consumir último dado do arquivo
+	Essa função é a que salva os dados lidos no arquivo de saída e termina
+	a execução, desalocando e fechando quem precisa. */
 void far salva_dados() {
+	/* Arquivo input não será mais necessário. */
 	fclose(input);
+	/*  Salvar no arquivo de saída os dados do buffer que armazenou os valores
+		recebidos do servidor. */
 	fwrite(download_servidor, sizeof(unsigned char), size, output);
+	/* Buffer não é mais necessário */
 	free(download_servidor);
+	/* Fechar últimos dois arquivos */
 	fclose(output);
 	fclose(log);
 }
 
 void far main() {
-	log = fopen("LOGPRCN.txt", "w");
-	input = fopen("IMGIN.jpg", "rb");
-	output = fopen("IMGOUT.jpg", "wb");
+	log = fopen("LOGPRCN.txt", "w"); /* modo de acesso: escrita */
+	input = fopen("IMGIN.jpg", "rb"); /* modo de acesso: leitura de arquivo binário */
+	output = fopen("IMGOUT.jpg", "wb"); /* modo de acesso: escrita de arquivo binário */
 	configurar();
 	iniciar_PRODCONS(max_buffer, size, servidor, cliente, salva_dados);
 }
 
+
+/* -------------------------- TESTE -------------------------*/
+/*
+FILE *input, *output, *log;
+int tamanho = 200, max_buffer = 20, pos_prod = 0, pos_cons = 0;
+char it_serv = 96;
+
+unsigned char far func_produz() {
+	fprintf(log, "Produziu (%d): %c\n", pos_prod++, ++it_serv);
+	return it_serv;
+}
+
+void far func_consome(unsigned char lido) {
+	fprintf(log, "Consumiu (%d): %c\n", pos_cons++, lido);
+}
+
+void far func_retorna() {
+	printf("ACABOU");
+}
+
+void far main() {
+	log = fopen("LOGPRCN.txt", "w");
+	iniciar_PRODCONS(max_buffer, tamanho, func_produz, func_consome, func_retorna);
+}
+*/
--- PRODCONS.C
+++ PRODCONS.C
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <nucleo.h>
 
+/* 	Declaração das funções que serão chamadas dentro do produtor/consumidor
+	e que terão valor atribuído através do parâmetro passado ao void iniciar_PRODCONS */
 unsigned char far (*produz)();
 void far (*consome)(unsigned char lido); 
 void far (*retorno)();
+
+/* Iteradores do produtor e consumidor */
 int prox_prod = 0, prox_cons = 0;
+
+/*  max = tamanho máximo do buffer
+	n = número de iterações a serem executadas */
 long unsigned int max, n;
+
+/*  Declaração dos semáforos.
+	cheia = células cheias do buffer. Deve ser iniciado em 0.
+	vazia = células vazias do buffer. Deve ser iniciado com max.
+	mutex = exclusão mútua do buffer. Deve ser iniciado em 1.
 semaforo cheia, vazia, mutex;
+
+/* Buffer compartilhado */
 unsigned char *buffer;
 
 void far produtor() {
@@ -15,6 +29,7 @@ void far produtor() {
 		P(&vazia);
 		P(&mutex);
 		buffer[prox_prod] = produz();
+		/* Se prox_prod > max, prox_prod = 0; */
 		prox_prod = (prox_prod + 1) % max;
 		V(&mutex);
 		V(&cheia);
@@ -28,31 +43,48 @@ void far consumidor() {
 		P(&cheia);
 		P(&mutex);
 		consome(buffer[prox_cons]);
+		/* Se prox_cons > max, prox_cons = 0; */
 		prox_cons = (prox_cons + 1) % max;
 		V(&mutex);
 		V(&vazia);
 	}
+	/* Desalocação do buffer compartilhado */
 	free(buffer);
+
+	/*  Função usada para salvar algum dado que se ache necessário ou qualquer
+		outra coisa a ser feita antes de ser chamado o volta_dos */
 	retorno();
 	termina_processo();
 }
 
-void iniciar_PRODCONS(long unsigned int max_buffer, 
-					  long unsigned int num_it, 
-					  unsigned char far (*func_produz)(), 
-					  void far (*func_consome)(unsigned char lido),
-					  void far (*func_retorno)()) {
+void iniciar_PRODCONS(long unsigned int max_buffer, /* Tamanho do buffer compartilhado */
+					  long unsigned int num_it, /* Número máximo de iterações */
+					  unsigned char far (*func_produz)(), /* Função chamada pelo produtor. Retornará valor a ser inserido no buffer. */
+					  void far (*func_consome)(unsigned char lido), /* Função chamada pelo consumidor. Executa alguma ação com o valor retirado do buffer. */
+					  void far (*func_retorno)()) { /* Função chamada quando consumidor termina de ler dados do produtor, isto é, quando chega no máximo de iterações e sai do for. */
+	/* Configuração das constantes */
 	max = max_buffer;
 	n = num_it;
-	buffer = (unsigned char *) calloc (max_buffer, 1);
+
+	/* Alocação do buffer usado pelo prod/cons */
+	buffer = (unsigned char *) calloc (max_buffer, sizeof(unsigned char));
+
+	/* 	Setar funções que serão chamadas pelo produtor/consumidor e que
+		vieram através de chamada externa à interface do produtor/consumidor. */ 
 	produz = func_produz;
 	consome = func_consome;
 	retorno = func_retorno;
+
+	/* Inicialização dos semáforos com os valores necessários */
 	inicia_semaforo(&cheia, 0);
 	inicia_semaforo(&vazia, max_buffer);
 	inicia_semaforo(&mutex, 1);
+
+	/* Adição dos processos na fila de processos prontos para serem alocados */
 	cria_processo(produtor, "prod");
 	cria_processo(consumidor, "cons");
+
+	/* Iniciar */
 	dispara_sistema();
 }
 
